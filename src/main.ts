bcf7935e25b33960e98a38173d23b4ae97481df3
Tiqur <trevorbrage02@gmail.com>
2023-04-16 13:11:40 -0700

    Get user oauth token

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const API_URL = 'https://osu.ppy.sh/api/v2';
-console.log(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+const OATH_TOKEN_URL = 'https://osu.ppy.sh/oauth/token';
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+async function getUserOAUTHToken(): Promise<string> {
+  const resp = await axios.post(OATH_TOKEN_URL, {
+    client_id: client_id,
+    client_secret: client_secret,
+    grant_type: 'client_credentials',
+    scope: 'public'
+  });
+
+  return resp.data.access_token;
+}
+
+(async () => {
+  const token = await getUserOAUTHToken();
+  console.log(token);
+})()
+
