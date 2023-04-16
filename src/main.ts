387cfbe27c1d9e3522f0176202d638692c496647
Tiqur <trevorbrage02@gmail.com>
2023-04-16 13:04:24 -0700

    Read environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log('test');
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+// Read enviornment variables
+dotenv.config();
+
+const API_URL = 'https://osu.ppy.sh/api/v2';
+console.log(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
